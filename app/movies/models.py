204dfeb487ebbac5a8f221fc80a257cf7b2fd0cd
jun-hf <fong.junh@gmail.com>
2022-10-10 13:38:03 +0100

    Code[Movie Model] - Write and passed movie model

--- app/movies/models.py
+++ app/movies/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models 
 
 # Create your models here.
 
@@ -6,3 +7,12 @@ class CustomUser(AbstractUser):
     pass 
 
 
+class Movie(models.Model):
+    title = models.CharField(max_length=225)
+    genre = models.CharField(max_length=255)
+    year = models.CharField(max_length=255)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title}"
