a83c070fdb1392af424e4d0bfc8bd36bbdff46a7
jun-hf <fong.junh@gmail.com>
2022-10-10 14:41:58 +0100

    Code[API Put]

--- app/movies/views.py
+++ app/movies/views.py
@@ -33,6 +33,14 @@ class MovieDetail(APIView):
         serializer = MovieSerializer(movie)
         return Response(serializer.data)
 
+    def put(self, request, pk, format=None):
+        movie = self.get_object(pk)
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     def delete(self, request, pk, format=None):
         movie = self.get_object(pk)
         movie.delete()
